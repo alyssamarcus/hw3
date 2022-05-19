618a80697aa3556ab1589539d8dcd2ca2eea8ba8
alyssamarcus <alyssa.marcus@kellogg.northwestern.edu>
2022-05-19 05:57:42 +0000

    updated

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,14 +2,18 @@ class PostsController < ApplicationController
 
     def new
         @post = Post.new
+        @post["place_id"] = params["place_id"]
+        @place = Place.find_by({"id" => params["place_id"]})
     end
     
     def create
         @post = Post.new
-        @post["name"] = params["post"]["name"]
-        @post["date"] = params["post"]["date"]
+        @post["title"] = params["post"]["title"]
+        @post["posted_on"] = params["post"]["posted_on"]
+        @post["description"] = params["post"]["description"]
+        @post["place_id"] = params["post"]["place_id"]
         @post.save
-        redirect_to "/posts"
+        redirect_to "/places/#{@post["place_id"]}"
     end
-    
-end
+
+end 
